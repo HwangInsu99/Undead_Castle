3e97f4d8316832b411f958deab0a1ab1c73a38de
HwangInsu99 <158982721+HwangInsu99@users.noreply.github.com>
2026-01-26 16:52:48 +0900

    Update CameraZoom.cs

--- Start&Option/CameraZoom.cs
+++ Start&Option/CameraZoom.cs
@@ -19,7 +19,28 @@ public class CameraZoom : MonoBehaviour
 
     void Update()
     {
-        float scroollWheel = Input.GetAxis("Mouse ScrollWheel");
+        float zoomDelta = 0f;
+#if UNITY_STANDALONE || UNITY_EDITOR
+        zoomDelta = Input.GetAxis("Mouse ScroolWheel") * scrollSpeed * Time.deltaTime;
+
+#elif UNITY_ANDROID || UNITY_IOS
+        if (Input.touchCount == 2)
+        {
+            Touch touch0 = Input.GetTouch(0);
+            Touch touch1 = Input.GetTouch(1);
+
+            Vector2 prevTouch0 = touch0.position - touch0.deltaPosition;
+            Vector2 prevTouch1 = touch1.position - touch1.deltaPosition;
+
+            float prevMagnitude = (prevTouch0 - prevTouch1).magnitude;
+            float currentMagnitude = (touch0.position - touch1.position).magnitude;
+
+            float difference = currentMagnitude - prevMagnitude;
+
+            zoomDelta = -difference * scrollSpeed * 0.001f * Time.deltaTime;
+        }
+#endif
+        /*float scroollWheel = Input.GetAxis("Mouse ScrollWheel");
         Camera.main.fieldOfView -= scroollWheel * Time.deltaTime * scrollSpeed;
 
         if (Camera.main.fieldOfView <=60 && Camera.main.fieldOfView >=45)
@@ -38,6 +59,6 @@ public class CameraZoom : MonoBehaviour
         {
             Camera.main.fieldOfView = 60;
             transform.position = Min;
-        }
+        }*/
     }
 }
